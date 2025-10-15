4921b6a0768b79d5349dc22749f8aa5d53b72fe9
Majitelll <romanek.kora@gmail.com>
2025-10-15 16:28:27 +0200

    first commit

---
+++ vendors/cglm/CMakeFiles/cglm.dir/DependInfo.cmake
@@ -0,0 +1,76 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dev/minecraft/vendors/cglm/src/aabb2d.c" "vendors/cglm/CMakeFiles/cglm.dir/src/aabb2d.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/aabb2d.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/affine.c" "vendors/cglm/CMakeFiles/cglm.dir/src/affine.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/affine.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/affine2d.c" "vendors/cglm/CMakeFiles/cglm.dir/src/affine2d.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/affine2d.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/bezier.c" "vendors/cglm/CMakeFiles/cglm.dir/src/bezier.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/bezier.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/box.c" "vendors/cglm/CMakeFiles/cglm.dir/src/box.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/box.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/cam.c" "vendors/cglm/CMakeFiles/cglm.dir/src/cam.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/cam.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/ortho_lh_no.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/ortho_lh_no.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/ortho_lh_no.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/ortho_lh_zo.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/ortho_lh_zo.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/ortho_lh_zo.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/ortho_rh_no.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/ortho_rh_no.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/ortho_rh_no.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/ortho_rh_zo.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/ortho_rh_zo.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/ortho_rh_zo.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/persp_lh_no.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/persp_lh_no.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/persp_lh_no.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/persp_lh_zo.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/persp_lh_zo.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/persp_lh_zo.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/persp_rh_no.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/persp_rh_no.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/persp_rh_no.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/persp_rh_zo.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/persp_rh_zo.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/persp_rh_zo.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/project_no.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/project_no.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/project_no.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/project_zo.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/project_zo.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/project_zo.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/view_lh_no.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/view_lh_no.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/view_lh_no.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/view_lh_zo.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/view_lh_zo.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/view_lh_zo.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/view_rh_no.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/view_rh_no.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/view_rh_no.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/clipspace/view_rh_zo.c" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/view_rh_zo.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/clipspace/view_rh_zo.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/curve.c" "vendors/cglm/CMakeFiles/cglm.dir/src/curve.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/curve.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/ease.c" "vendors/cglm/CMakeFiles/cglm.dir/src/ease.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/ease.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/euler.c" "vendors/cglm/CMakeFiles/cglm.dir/src/euler.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/euler.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/frustum.c" "vendors/cglm/CMakeFiles/cglm.dir/src/frustum.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/frustum.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/io.c" "vendors/cglm/CMakeFiles/cglm.dir/src/io.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/io.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/ivec2.c" "vendors/cglm/CMakeFiles/cglm.dir/src/ivec2.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/ivec2.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/ivec3.c" "vendors/cglm/CMakeFiles/cglm.dir/src/ivec3.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/ivec3.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/ivec4.c" "vendors/cglm/CMakeFiles/cglm.dir/src/ivec4.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/ivec4.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat2.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat2.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat2.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat2x3.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat2x3.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat2x3.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat2x4.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat2x4.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat2x4.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat3.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat3.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat3.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat3x2.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat3x2.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat3x2.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat3x4.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat3x4.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat3x4.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat4.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat4.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat4.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat4x2.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat4x2.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat4x2.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/mat4x3.c" "vendors/cglm/CMakeFiles/cglm.dir/src/mat4x3.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/mat4x3.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/noise.c" "vendors/cglm/CMakeFiles/cglm.dir/src/noise.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/noise.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/plane.c" "vendors/cglm/CMakeFiles/cglm.dir/src/plane.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/plane.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/project.c" "vendors/cglm/CMakeFiles/cglm.dir/src/project.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/project.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/quat.c" "vendors/cglm/CMakeFiles/cglm.dir/src/quat.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/quat.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/ray.c" "vendors/cglm/CMakeFiles/cglm.dir/src/ray.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/ray.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/sphere.c" "vendors/cglm/CMakeFiles/cglm.dir/src/sphere.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/sphere.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/vec2.c" "vendors/cglm/CMakeFiles/cglm.dir/src/vec2.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/vec2.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/vec3.c" "vendors/cglm/CMakeFiles/cglm.dir/src/vec3.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/vec3.c.o.d"
+  "/home/dev/minecraft/vendors/cglm/src/vec4.c" "vendors/cglm/CMakeFiles/cglm.dir/src/vec4.c.o" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/src/vec4.c.o.d"
+  "" "vendors/cglm/libcglm.so" "gcc" "vendors/cglm/CMakeFiles/cglm.dir/link.d"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/dev/minecraft/vendors/cglm/libcglm.so" "/home/dev/minecraft/vendors/cglm/libcglm.so.0.9.6"
+  "/home/dev/minecraft/vendors/cglm/libcglm.so.0" "/home/dev/minecraft/vendors/cglm/libcglm.so.0.9.6"
+  )
+
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
